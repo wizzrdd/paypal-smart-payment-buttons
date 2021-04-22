061ea627fdda6d41d7328dbf513bb514258807a7
Steve Mask <smask@paypal.com>
2021-04-22 15:38:05 -0400

    types

--- src/button/props.js
+++ src/button/props.js
@@ -4,7 +4,7 @@ import type { CrossDomainWindowType } from 'cross-domain-utils/src';
 import { ENV, INTENT, COUNTRY, FUNDING, CARD, PLATFORM, CURRENCY, type FundingEligibilityType } from '@paypal/sdk-constants/src';
 import { ZalgoPromise } from 'zalgo-promise/src';
 import type { InstallmentsFlowType } from '@paypal/installments/src/types';
-import type { ApplePaySessionConfig } from '@paypal/checkout-components/src/ui/buttons/props';
+import type { ApplePaySessionConfigRequest } from '@paypal/checkout-components/src/ui/buttons/props';
 
 import type { ContentType, LocaleType, ProxyWindow, Wallet, CheckoutFlowType, CardFieldsFlowType,
     ThreeDomainSecureFlowType, MenuFlowType, ConnectOptions, PersonalizationType } from '../types';
@@ -105,7 +105,7 @@ export type ButtonXProps = {|
     branded : boolean,
     userExperienceFlow : string,
 
-    applePay : ApplePaySessionConfig
+    applePay : ApplePaySessionConfigRequest
 |};
 
 export type ButtonProps = {|
@@ -173,7 +173,7 @@ export type ButtonProps = {|
     branded : boolean,
     userExperienceFlow : string,
 
-    applePay : ApplePaySessionConfig
+    applePay : ApplePaySessionConfigRequest
 |};
 
 // eslint-disable-next-line complexity
