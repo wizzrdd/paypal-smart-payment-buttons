0eab43e114f7563df72cb06e55517c4f601c8990
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2021-05-28 11:19:09 -0500

    Log when the latest dist tag has been downloaded

--- server/components/buttons/script.js
+++ server/components/buttons/script.js
@@ -3,12 +3,14 @@
 import { join, dirname } from 'path';
 import { readFileSync } from 'fs';
 
+import { noop } from 'belter';
 import { ENV } from '@paypal/sdk-constants';
 
 import type { CacheType } from '../../types';
 import { BUTTON_RENDER_JS, BUTTON_CLIENT_JS, SMART_BUTTONS_MODULE, CHECKOUT_COMPONENTS_MODULE,
-    BUTTON_CLIENT_MIN_JS, WEBPACK_CONFIG, ACTIVE_TAG } from '../../config';
-import { isLocalOrTest, compileWebpack, babelRequire, evalRequireScript, resolveScript, dynamicRequire, type LoggerBufferType } from '../../lib';
+    BUTTON_CLIENT_MIN_JS, WEBPACK_CONFIG, ACTIVE_TAG, LATEST_TAG } from '../../config';
+import { isLocalOrTest, compileWebpack, babelRequire, evalRequireScript, resolveScript,
+    dynamicRequire, type LoggerBufferType } from '../../lib';
 import { getPayPalSDKWatcher, getPayPalSmartPaymentButtonsWatcher } from '../../watchers';
 
 const ROOT = join(__dirname, '../../..');
@@ -55,7 +57,7 @@ export async function getPayPalSmartPaymentButtonsRenderScript({ logBuffer, cach
             return script;
         }
     }
-    
+
     const watcher = getPayPalSDKWatcher({ logBuffer, cache });
     const { version } = await watcher.get(ACTIVE_TAG);
     const button = await watcher.importDependency(CHECKOUT_COMPONENTS_MODULE, BUTTON_RENDER_JS, ACTIVE_TAG);
@@ -102,6 +104,10 @@ export async function getSmartPaymentButtonsClientScript({ logBuffer, cache, deb
     const watcher = getPayPalSmartPaymentButtonsWatcher({ logBuffer, cache });
     const { version } = await watcher.get(ACTIVE_TAG);
     const script = await watcher.read(debug ? BUTTON_CLIENT_JS : BUTTON_CLIENT_MIN_JS, ACTIVE_TAG);
+
+    watcher.get(LATEST_TAG).then(({ version: deployVersion }) => {
+        logBuffer.info(`deploy_client_version_${ deployVersion.replace(/[^0-9]+/g, '_') }`, { deployVersion });
+    }).catch(noop);
 
     return { script, version };
 }
