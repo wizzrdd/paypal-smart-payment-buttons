8cc1e876c3d5fa8491013afc621a74fdba54d1ac
Steve Mask <smask@paypal.com>
2021-06-08 15:27:28 -0400

    remove upgradeLSAT

--- src/props/onAuth.js
+++ src/props/onAuth.js
@@ -5,8 +5,7 @@ import { stringifyError } from 'belter/src';
 
 import { upgradeFacilitatorAccessToken } from '../api';
 import { getLogger } from '../lib';
-import { upgradeLSATExperiment } from '../experiments';
-import { LSAT_UPGRADE_EXCLUDED_MERCHANTS } from '../constants';
+import { LSAT_UPGRADE_EXCLUDED_MERCHANTS, LSAT_UPGRADE_RESULT_KEY } from '../constants';
 
 import type { CreateOrder } from './createOrder';
 
@@ -19,31 +18,29 @@ export type OnAuth = (params : XOnAuthDataType) => ZalgoPromise<string | void>;
 type GetOnAuthOptions = {|
     facilitatorAccessToken : string,
     createOrder : CreateOrder,
-    upgradeLSAT : boolean,
-    userIDToken : ?string,
     clientID : string
 |};
 
-export function getOnAuth({ facilitatorAccessToken, createOrder, upgradeLSAT, userIDToken, clientID } : GetOnAuthOptions) : OnAuth {
-    upgradeLSAT = upgradeLSAT && (Boolean(userIDToken) || upgradeLSATExperiment.isEnabled()) && LSAT_UPGRADE_EXCLUDED_MERCHANTS.indexOf(clientID) === -1;
+export function getOnAuth({ facilitatorAccessToken, createOrder, clientID } : GetOnAuthOptions) : OnAuth {
+    const upgradeLSAT = LSAT_UPGRADE_EXCLUDED_MERCHANTS.indexOf(clientID) === -1;
 
     return ({ accessToken } : XOnAuthDataType) => {
         getLogger().info(`spb_onauth_access_token_${ accessToken ? 'present' : 'not_present' }`);
 
         return ZalgoPromise.try(() => {
             if (accessToken) {
-                upgradeLSATExperiment.logStart();
-
                 if (upgradeLSAT) {
                     return createOrder()
                         .then(orderID => upgradeFacilitatorAccessToken(facilitatorAccessToken, { buyerAccessToken: accessToken, orderID }))
                         .then(() => {
                             getLogger().info('upgrade_lsat_success');
+                            window[LSAT_UPGRADE_RESULT_KEY] = true;
 
                             return accessToken;
                         })
                         .catch(err => {
                             getLogger().warn('upgrade_lsat_failure', { error: stringifyError(err) });
+                            window[LSAT_UPGRADE_RESULT_KEY] = false;
 
                             return accessToken;
                         });
