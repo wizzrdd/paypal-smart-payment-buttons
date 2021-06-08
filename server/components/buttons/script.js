0bc7b75f5bdbeb765138d106c8df173f87c9ef84
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2021-06-08 17:50:10 -0500

    Add logging for all watcher calls

--- server/components/buttons/script.js
+++ server/components/buttons/script.js
@@ -8,7 +8,7 @@ import { ENV } from '@paypal/sdk-constants';
 
 import type { CacheType } from '../../types';
 import { BUTTON_RENDER_JS, BUTTON_CLIENT_JS, SMART_BUTTONS_MODULE, CHECKOUT_COMPONENTS_MODULE,
-    BUTTON_CLIENT_MIN_JS, WEBPACK_CONFIG, ACTIVE_TAG, LATEST_TAG } from '../../config';
+    BUTTON_CLIENT_MIN_JS, WEBPACK_CONFIG, ACTIVE_TAG } from '../../config';
 import { isLocalOrTest, compileWebpack, babelRequire, evalRequireScript, resolveScript,
     dynamicRequire, type LoggerBufferType } from '../../lib';
 import { getPayPalSDKWatcher, getPayPalSmartPaymentButtonsWatcher } from '../../watchers';
@@ -58,9 +58,13 @@ export async function getPayPalSmartPaymentButtonsRenderScript({ logBuffer, cach
         }
     }
 
-    const watcher = getPayPalSDKWatcher({ logBuffer, cache });
-    const { version } = await watcher.get(ACTIVE_TAG);
-    const button = await watcher.importDependency(CHECKOUT_COMPONENTS_MODULE, BUTTON_RENDER_JS, ACTIVE_TAG);
+    const { getTag, getDeployTag, importDependency } = getPayPalSDKWatcher({ logBuffer, cache });
+    const { version } = await getTag();
+    const button = await importDependency(CHECKOUT_COMPONENTS_MODULE, BUTTON_RENDER_JS, ACTIVE_TAG);
+
+    // non-blocking download of the DEPLOY_TAG
+    getDeployTag().catch(noop);
+
     return { button, version };
 }
 
@@ -101,13 +105,12 @@ export async function getSmartPaymentButtonsClientScript({ logBuffer, cache, deb
         }
     }
 
-    const watcher = getPayPalSmartPaymentButtonsWatcher({ logBuffer, cache });
-    const { version } = await watcher.get(ACTIVE_TAG);
-    const script = await watcher.read(debug ? BUTTON_CLIENT_JS : BUTTON_CLIENT_MIN_JS, ACTIVE_TAG);
+    const { getTag, getDeployTag, read } = getPayPalSmartPaymentButtonsWatcher({ logBuffer, cache });
+    const { version } = await getTag();
+    const script = await read(debug ? BUTTON_CLIENT_JS : BUTTON_CLIENT_MIN_JS, ACTIVE_TAG);
 
-    watcher.get(LATEST_TAG).then(({ version: deployVersion }) => {
-        logBuffer.info(`deploy_client_version_${ deployVersion.replace(/[^0-9]+/g, '_') }`, { deployVersion });
-    }).catch(noop);
+    // non-blocking download of the DEPLOY_TAG
+    getDeployTag().catch(noop);
 
     return { script, version };
 }
